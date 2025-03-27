e1d31302815fe448474bffd446eeb18da94c3c2b
kzhgit <qiandaoyisui@gmail.com>
2025-03-27 12:23:39 +0900

    fix: Update response message to include additional exclamation marks

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Cloud Run!');
+  res.send('Hello from Cloud Run！！！！');
 });
 
 app.listen(port, () => {
